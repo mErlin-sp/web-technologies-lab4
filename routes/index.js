050311868b27bbcd16a836ebfabcc8c5049bf7d5
Oleksandr <merlin.ua.1@gmail.com>
2023-11-01 02:23:31 +0200

    Add footer

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Lab4. Oleksandr Popov. IT-z01. Variant 24 (4)'});
 });
 
 module.exports = router;
